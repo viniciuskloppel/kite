5475a362d7bd9c26a481ad3eb06fa044597a5285
Mike MacCana <mike.maccana@gmail.com>
2025-02-13 12:35:57 -0500

    Exclude the one real-value test and add reason

--- src/tests/smart-transactions.test.ts
+++ src/tests/smart-transactions.test.ts
@@ -10,27 +10,31 @@ describe("Smart transactions", () => {
   // MAINNET_WALLET_WITH_SOME_SOL
   // MAINNET_WALLET_THAT_WILL_RECIEVE_SOL
   // HELIUS_API_KEY
-  test("transferLamports on mainnet with retries", async () => {
-    dotenv.config();
-    const connection = connect("helius-mainnet");
-    const sender = await loadWalletFromEnvironment("MAINNET_WALLET_WITH_SOME_SOL");
-    const recipient = await loadWalletFromEnvironment("MAINNET_WALLET_THAT_WILL_RECIEVE_SOL");
+  test(
+    "transferLamports on mainnet with retries",
+    { skip: "skipping mainnet tests unless explicitly needed" },
+    async () => {
+      dotenv.config();
+      const connection = connect("helius-mainnet");
+      const sender = await loadWalletFromEnvironment("MAINNET_WALLET_WITH_SOME_SOL");
+      const recipient = await loadWalletFromEnvironment("MAINNET_WALLET_THAT_WILL_RECIEVE_SOL");
 
-    try {
-      const transferSignature = await connection.transferLamports({
-        source: sender,
-        destination: recipient.address,
-        // A tiny amount to test the retries since this is mainnet and the SOL has value
-        amount: lamports(100n),
-        skipPreflight: true,
-        maximumRetries: 4,
-      });
+      try {
+        const transferSignature = await connection.transferLamports({
+          source: sender,
+          destination: recipient.address,
+          // A tiny amount to test the retries since this is mainnet and the SOL has value
+          amount: lamports(100n),
+          skipPreflight: true,
+          maximumRetries: 4,
+        });
 
-      assert.ok(transferSignature);
-    } catch (thrownObject) {
-      const error = thrownObject as Error;
-      console.log(error.message);
-      console.log(error.cause);
-    }
-  });
+        assert.ok(transferSignature);
+      } catch (thrownObject) {
+        const error = thrownObject as Error;
+        console.log(error.message);
+        console.log(error.cause);
+      }
+    },
+  );
 });
