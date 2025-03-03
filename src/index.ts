70df0ac8136f5b6349fff53d45620a08f69c33f3
Mike MacCana <mike.maccana@gmail.com>
2025-03-03 12:25:10 -0500

    Export more useful variables

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { connect } from "./lib/connect";
-export { SOL } from "./lib/constants";
+export { SOL, TOKEN_PROGRAM, TOKEN_EXTENSIONS_PROGRAM, ASSOCIATED_TOKEN_PROGRAM } from "./lib/constants";
 export type { Connection } from "./lib/connect";
