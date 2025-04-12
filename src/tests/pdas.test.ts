29d7be14289d6b7831692eef0e9344c268e53aab
Mike MacCana <mike.maccana@gmail.com>
2025-04-12 15:54:46 -0400

    Remvoe unneccessarily verbose comment

--- src/tests/pdas.test.ts
+++ src/tests/pdas.test.ts
@@ -4,25 +4,6 @@ import { Address } from "@solana/kit";
 import { getPDAAndBump } from "../lib/pdas";
 
 describe("getPDAAndBump", () => {
-  // This is the web3.js v1 equivalent code:
-  //
-  //   import { PublicKey } from "@solana/kit";
-  //   import { BN } from "bn.js";
-
-  //   const aliceAddress = new PublicKey("qbuMdeYxYJXBjU6C6qFKjZKjXmrU83eDQomHdrch826");
-
-  //   const programAddress = new PublicKey("CXSfSLuAf6gtkGBH6i1kyRXFtH7CqNRHnPMo39AFKn6X");
-
-  //   const offerId = new BN(420);
-
-  //   const [offer, bump] = PublicKey.findProgramAddressSync(
-  //     [Buffer.from("offer"), aliceAddress.toBuffer(), offerId.toArrayLike(Buffer, "le", 8)],
-  //     programAddress,
-  //   );
-
-  //   console.log(offer.toBase58());
-  //   console.log(bump);
-
   test("getPDAAndBump returns the same as the web3.js v1 equivalent code", async () => {
     const aliceAddress = "qbuMdeYxYJXBjU6C6qFKjZKjXmrU83eDQomHdrch826" as Address;
     const programAddress = "CXSfSLuAf6gtkGBH6i1kyRXFtH7CqNRHnPMo39AFKn6X" as Address;
