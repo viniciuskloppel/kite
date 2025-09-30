6151f339cd1e0164f8d8939c7a80a3f3cf482905
amilz <85324096+amilz@users.noreply.github.com>
2025-09-29 17:55:02 -0700

    chore: remove no longer relevant test
    this test no longer makes sense given that we handle simulation error in our retry logic

--- src/tests/transactions.test.ts
+++ src/tests/transactions.test.ts
@@ -1,14 +1,7 @@
 import { describe, test, before } from "node:test";
 import assert from "node:assert";
 import { connect, Connection } from "..";
-import {
-  isSolanaError,
-  KeyPairSigner,
-  lamports,
-  SOLANA_ERROR__JSON_RPC__SERVER_ERROR_SEND_TRANSACTION_PREFLIGHT_FAILURE,
-  SOLANA_ERROR__TRANSACTION_ERROR__ALREADY_PROCESSED,
-  SolanaError,
-} from "@solana/kit";
+import { KeyPairSigner, lamports } from "@solana/kit";
 import { getTransferSolInstruction } from "@solana-program/system";
 import { SOL } from "../lib/constants";
 
@@ -113,30 +106,5 @@ describe("Send Transaction From Instructions", () => {
       commitment: "finalized",
     });
     assert.ok(signature, "Transaction should return a signature");
-  });
-  test("should fail if short timeout is used (finalized commitment)", async () => {
-    const transferInstruction = getTransferSolInstruction({
-      source: sender,
-      destination: recipient.address,
-      amount: lamports(100_000n), // 0.0001 SOL
-    });
-
-    const promise = connection.sendTransactionFromInstructions({
-      feePayer: sender,
-      instructions: [transferInstruction],
-      maximumClientSideRetries: 3,
-      commitment: "finalized",
-      timeout: 1000,
-    });
-    await assert.rejects(promise, (error) => {
-      // depending on timing, we could get a simulation error or an already processed error
-      // we expect one of these errors
-      const isSimulationError = isSolanaError(
-        error,
-        SOLANA_ERROR__JSON_RPC__SERVER_ERROR_SEND_TRANSACTION_PREFLIGHT_FAILURE,
-      );
-      const isAlreadyProcessedError = isSolanaError(error, SOLANA_ERROR__TRANSACTION_ERROR__ALREADY_PROCESSED);
-      return isSimulationError || isAlreadyProcessedError;
-    });
   });
 });
